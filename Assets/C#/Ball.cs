5ce48d94aecdc46ffff6153dcbdcc3f7a04fc9fd
Toni Sundell <toni.sundell@cs.tamk.fi>
2018-03-08 17:28:49 +0200

    Ball and Kart pulse

---
+++ Assets/C#/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField]
+    private float _minSpeed;
+
+    public Rigidbody Rb;
+
+    private void Awake()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Rb.velocity.magnitude < _minSpeed)
+        {
+            Rb.velocity = Rb.velocity.normalized * _minSpeed;
+        }
+    }
+}
--- Assets/C#/KartPulse.cs
+++ Assets/C#/KartPulse.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class KartPulse : MonoBehaviour
 {
-    public float pulseRadius;
+    public Transform PulseOrigin;
+    public float PulseRadius;
+    public float PulseForce;
 
-    
+    public void Pulse()
+    {
+        Collider[] colliders = Physics.OverlapSphere(PulseOrigin.position, PulseRadius, 8, QueryTriggerInteraction.Ignore);
+        Ball[] balls = new Ball[colliders.Length];
+        for (int i = 0; i < balls.Length; i++)
+        {
+            balls[i] = colliders[i].GetComponent<Ball>();
+        }
+
+        foreach (Ball ball in balls)
+        {
+            ball.Rb.AddForce((PulseOrigin.position - ball.transform.position).normalized * PulseForce);
+        }
+    }
 }
