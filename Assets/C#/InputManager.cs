cbe08f47ad59fcbd2547cb75fe134a3a3ed378e5
Toni Sundell <toni.sundell@cs.tamk.fi>
2018-03-09 14:50:22 +0200

    Stopped tracking "Library". Added player and tweeked input manager for testing.

--- Assets/C#/InputManager.cs
+++ Assets/C#/InputManager.cs
@@ -7,14 +7,32 @@ public class InputManager : MonoBehaviour {
     private KartMovement[] _kartMovements;
     private KartPulse[] _kartPulse;
 
+    public string[] ControllerNames;
+    public Player[] Players;
+
 	// Use this for initialization
 	void Start () {
         _kartMovements = FindObjectsOfType<KartMovement>();
         _kartPulse = FindObjectsOfType<KartPulse>();
+
+        Player[] unorderedPlayers = FindObjectsOfType<Player>();
+        Players = new Player[unorderedPlayers.Length];
+        foreach (Player player in unorderedPlayers)
+        {
+            Players[(int)player.currentPlayer - 1] = player;
+        }
+
+        PairConrollersToPlayers();
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (ControllerNames.Length != Input.GetJoystickNames().Length)
+        {
+            PairConrollersToPlayers();
+        }
+        
         //Player one
         float player1_input = Input.GetAxisRaw("HorizontalP1");
         float player2_input = Input.GetAxisRaw("HorizontalP2");
@@ -36,6 +54,22 @@ public class InputManager : MonoBehaviour {
             Debug.Log("pulse2 fired");
             _kartPulse[1].Pulse();
             
+        }
+    }
+
+    private void PairConrollersToPlayers()
+    {
+        ControllerNames = Input.GetJoystickNames();
+
+        foreach (Player player in Players)
+        {
+            player.controllerName = null;
+        }
+
+        for (int i = 0; i < ControllerNames.Length; i++)
+        {
+            ControllerNames[i] = "Controller " + i;
+            Players[i].controllerName = ControllerNames[i];
         }
     }
 }
