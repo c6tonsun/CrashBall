fdf40c351035e921ad718e728b9aafb39a96f63c
Toni Sundell <toni.sundell@cs.tamk.fi>
2018-03-09 17:24:30 +0200

    Added option to ball for fixed y position.

--- Assets/C#/Ball.cs
+++ Assets/C#/Ball.cs
@@ -6,6 +6,9 @@ public class Ball : MonoBehaviour {
 
     [SerializeField]
     private float _minSpeed;
+    private bool _isFixedY = false;
+    private float _fixedY = 0f;
+    public bool stayOnFloor = false;
 
     [HideInInspector]
     public Rigidbody Rb;
@@ -20,6 +23,20 @@ public class Ball : MonoBehaviour {
         if (Rb.velocity.magnitude < _minSpeed)
         {
             Rb.velocity = Rb.velocity.normalized * _minSpeed;
+        }
+
+        if (_isFixedY) transform.position = new Vector3(transform.position.x, _fixedY, transform.position.z);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_isFixedY || !stayOnFloor) return;
+
+        Transform floor = other.transform;
+        if (floor.name == "Floor")
+        {
+            _fixedY = floor.position.y + (floor.localScale.y * 0.5f) + (transform.localScale.y * 0.5f);
+            _isFixedY = true;
         }
     }
 }
