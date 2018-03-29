5110424c1c570fa700f2d25436744ca943e2dbd6
Toni Sundell <toni.sundell@cs.tamk.fi>
2018-03-29 13:18:13 +0300

    Stun bug fix.

---
+++ Assets/C#/CameraShake.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private Vector3 _defaultPos;
+    private Vector3 _defaultRot;
+
+    public bool shake;
+
+    private void Awake()
+    {
+        _defaultPos = transform.position;
+        _defaultRot = transform.eulerAngles;
+    }
+}
