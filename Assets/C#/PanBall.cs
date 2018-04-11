ae070f9cf491b2084ab6d4d1180538fa5c3336d1
Max Louhio <mlouhio@hotmail.com>
2018-04-11 16:50:15 +0300

    Tele fucking visions

---
+++ Assets/C#/PanBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanBall : Ball {
+
+    [SerializeField]
+    private GameObject DeathParticles;
+    private float particleDuration;
+
+    private void Start()
+    {
+        particleDuration = DeathParticles.GetComponent<ParticleSystem>().main.duration+0.5f;
+    }
+
+    new void FixedUpdate () {
+        base.FixedUpdate();
+        if (!canScore)
+        {
+            Destroy(Instantiate(DeathParticles, transform.position, transform.rotation), particleDuration);
+            gameObject.SetActive(false);
+        }
+	}
+}
