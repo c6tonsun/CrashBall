da7d31423cbd77c6bf3558ea33d4ea76ac37641c
Max Louhio <mlouhio@hotmail.com>
2018-05-16 18:36:58 +0300

    Added breathing script which rescales whatever it's stuck to. Highlights now change colors after colors have been picked.

---
+++ Assets/HighlightColors.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightColors : MonoBehaviour {
+
+    GameManager _gameManager;
+    UIMenuHandler menuHandler;
+
+    [SerializeField]
+    ParticleSystem[] childParticles;
+    [SerializeField]
+    Vector3[] selectedSize;
+    [SerializeField]
+    MeshRenderer[] meshes;
+
+	// Use this for initialization
+	void Start () {
+        _gameManager = FindObjectOfType<GameManager>();
+        menuHandler = FindObjectOfType<UIMenuHandler>();
+
+        childParticles = GetComponentsInChildren<ParticleSystem>();
+
+        meshes = GetComponentsInChildren<MeshRenderer>();
+        selectedSize = new Vector3[meshes.Length];
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < selectedSize.Length; i++)
+        {
+            selectedSize[i] = meshes[i].transform.localScale;
+        }
+        for (int i = 0; i < childParticles.Length; i++)
+        {
+            var shape = childParticles[i].shape;
+            shape.scale = selectedSize[i];
+        }
+
+        if(menuHandler.activeMenu.allPlayersNeedToBeReady && !menuHandler.activeMenu.isColorPickMenu)
+        {
+            for(int m = 0; m<meshes.Length; m++)
+            {
+                meshes[m].material.color = _gameManager.Colors[m];
+
+                var main = childParticles[m].main;
+                main.startColor = _gameManager.Colors[m];
+            }
+
+        }
+	}
+}
